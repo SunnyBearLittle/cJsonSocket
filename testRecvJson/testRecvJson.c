6aaa1dc7106a3f97bb63e4970bc5a645136c84d9
Kelvin.Xiong <1024469625@qq.com>
2020-11-04 19:32:36 +0800

    添加获取指定字段

--- testRecvJson/testRecvJson.c
+++ testRecvJson/testRecvJson.c
@@ -2,15 +2,21 @@
 #include <winsock2.h>
 #include <math.h>
 #include <stdlib.h>
+#include <string.h>
 #pragma comment(lib,"..\\WS2_32.Lib")   //加载 ws2_32.dll
 #define maxnum  1000
 #define BUF_SIZE 500
 #define INFO_SIZE 15
 
+char returnData[100];
+
 int split_str(char* mySendStr);
+// 存储字段不同数据
 char* dataArrOne[INFO_SIZE];
 void sortOutPath(char* routerPathStr);
 int dataArrTwo[100][100];
+char* getData(char* dataStr);
+char* getPrev(char* dataStr);
 
 int main(int argc, char* argv[])
 {
@@ -19,6 +25,8 @@ int main(int argc, char* argv[])
 	SOCKADDR_IN sockAddr;
 	SOCKADDR clntAddr;
 	int nSize, arrSize, allSize, matrixSize,x,y;//matrixSize矩阵大小
+
+    
 	char buffer[BUF_SIZE];
 
 	WSAStartup(MAKEWORD(2, 2), &wsaData);
@@ -45,6 +53,10 @@ int main(int argc, char* argv[])
 		float result;
 		int dist[maxnum];//保存路径
 		int pre[maxnum];     // 记录当前点的前一个结点
+
+        char* equipmentName, *lanAffliation, *routerIp, *operatingSystem, *taskSystem, *area, *prev, *next;
+        char* deviceBrand, *transformRate,*routerProtocol, *jumpNumber,*linkDelay,*networkLevel;
+        
 		SOCKET clntSock = accept(servSock, (SOCKADDR*)&clntAddr, &nSize);
 		int strLen = recv(clntSock, buffer, BUF_SIZE, 0);
 		//send(clntSock, buffer, strLen, 0);
@@ -73,10 +85,42 @@ int main(int argc, char* argv[])
             // printf("%s\n",arrData);
             infoSize = split_str(arrData);
             // printf("info_size:%d\n", infoSize);
-            printf("routerPath:%s\n", dataArrOne[8]);
+            // printf("routerPath:%s\n", dataArrOne[8]);
 
             // 整理字符串为二维数组数据
             sortOutPath(dataArrOne[8]);
+
+            // 获取设备数据
+            // equipmentName = getData(dataArrOne[0]);
+            // printf("equipment:%s", equipmentName);
+
+            // lanAffliation  = getData(dataArrOne[1]);
+            // printf("lanAffliation:%s\n", lanAffliation);
+            // routerIp  = getData(dataArrOne[2]);
+            // printf("routerIp:%s\n", routerIp);
+            // operatingSystem  = getData(dataArrOne[3]);
+            // printf("operatingSystem:%s\n", operatingSystem);
+            // deviceBrand  = getData(dataArrOne[4]);
+            // printf("deviceBrand:%s\n", deviceBrand);
+            // transformRate  = getData(dataArrOne[5]);
+            // printf("transformRate:%s\n", transformRate);
+            // routerProtocol  = getData(dataArrOne[6]);
+            // printf("routerProtocol:%s\n", routerProtocol);
+            // jumpNumber  = getData(dataArrOne[7]);
+            // printf("jumpNumber:%s\n", jumpNumber);
+            // linkDelay  = getData(dataArrOne[9]);
+            // printf("linkDelay:%s\n", linkDelay);
+            // networkLevel  = getData(dataArrOne[10]);
+            // printf("networkLevel:%s\n", networkLevel);
+            // taskSystem  = getData(dataArrOne[11]);
+            // printf("taskSystem:%s\n", taskSystem);
+            // area  = getData(dataArrOne[12]);
+            // printf("area:%s\n", area);
+            // prev  = getPrev(dataArrOne[13]);
+            // next  = getPrev(dataArrOne[14]);
+            // printf("prev:%s\tnext:%s\n", prev, next);
+
+
             arrSize++;
         }
         
@@ -86,6 +130,45 @@ int main(int argc, char* argv[])
 	//终止 DLL 的使用
 	WSACleanup();
 	return 0;
+}
+
+//获取prev或next
+char* getPrev(char* dataStr){
+    
+    char* p;
+    // returnData = (char*)malloc(100);
+    p=strtok(dataStr,":");
+    p = strtok(NULL,":");
+    // printf("获取的后半段数据：%s\t", p);
+
+    // 去除中括号
+    p = strtok(p, "[");
+    // printf("去除一个左括号：%s\t", p);
+
+    p = strtok(p, "]");
+    // printf("去除一个右括号：%s\t", p);
+
+    // 去除上引号
+    p = strtok(p, "'");
+    p = strtok(p, "'");
+    // printf("%s\t",p);
+
+    // strcpy(returnData,p);
+    // printf("strcpy:%s\n", returnData);
+
+    return p;
+}
+
+// 获取设备数据
+char* getData(char* dataStr){
+    char* p;
+    p=strtok(dataStr,":");
+    p = strtok(NULL,":");
+    // 去除上引号
+    p = strtok(p, "'");
+    p = strtok(p, "'");
+    printf("%s\n",p);
+    return p;
 }
 
 // 清理路径数据
@@ -99,7 +182,7 @@ void sortOutPath(char* routerPathStr){
     p = strtok(p, "'");
     p = strtok(p, "'");
     
-    printf("%s\n",p);
+    // printf("%s\n",p);
     
 
     // 去除-号
@@ -118,6 +201,7 @@ void sortOutPath(char* routerPathStr){
     dataArrTwo[x][y] = weight;
 
 }
+
 
 // 切割字符串
 int split_str(char* mySendStr){
